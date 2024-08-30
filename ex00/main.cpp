460ec5fa7aabf7caa8287d07b79ef1caa2e979d8
jabayaki <jabayaki-system>
2024-08-30 18:34:00 +0900

    Add 'try', 'catch' for safety reasons

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,19 +1,23 @@
 #include "Bureaucrat.hpp"
 
 int	main() {
-	Bureaucrat	bure[] = {Bureaucrat("taro", 1), Bureaucrat("jon", 149)};
-	std::cout << bure[0] << std::endl;
-	std::cout << bure[1] << std::endl;
-
-	bure[0].incrementGrade();
-	std::cout << bure[0] << std::endl;
-	bure[0].decrementGrade();
-	std::cout << bure[0] << std::endl;
-
-	bure[1].decrementGrade();
-	std::cout << bure[1] << std::endl;
-	bure[1].decrementGrade();
-	std::cout << bure[1] << std::endl;
-
+	try {
+		Bureaucrat	bure[] = {Bureaucrat("taro", 1), Bureaucrat("jon", 149)};
+		std::cout << bure[0] << std::endl;
+		std::cout << bure[1] << std::endl;
+	
+		bure[0].incrementGrade();
+		std::cout << bure[0] << std::endl;
+		bure[0].decrementGrade();
+		std::cout << bure[0] << std::endl;
+	
+		bure[1].decrementGrade();
+		std::cout << bure[1] << std::endl;
+		bure[1].decrementGrade();
+		std::cout << bure[1] << std::endl;
+	}
+	catch (const std::exception& e) {
+		std::cerr << "An error occurred: " << e.what() << std::endl;
+	}
 	return 0;
 }
